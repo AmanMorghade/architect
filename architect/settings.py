eeb7c46377677ba7219c1712fb5c7a6d0252cfe5
AmanMorghade <amanmorghade1@gmail.com>
2025-07-14 11:55:54 +0530

    static file

--- architect/settings.py
+++ architect/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,6 +128,9 @@ CONTACT_EMAIL = 'shinchan.from.kasukabe@gmail.com'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # optional, for your app-level static
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
